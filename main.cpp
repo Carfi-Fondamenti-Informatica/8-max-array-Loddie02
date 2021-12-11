bd4dbec2326294c94fa8132141a4e6c45f3a12ec
Loddie02 <91880336+Loddie02@users.noreply.github.com>
2021-12-11 14:54:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+int main() {
+    int dim;
+    cin>>dim;
+    float numeri[dim];
+    for (int i=0; i<dim; i++){
+        cin>>numeri[i];
+    }
+    max(numeri,dim);
+        cout<<max(numeri,dim)<<endl;
+
+    return 0;
 }
