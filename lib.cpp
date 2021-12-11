c288afc931ae3e6497885f5182b5ebed17bff5f3
Loddie02 <91880336+Loddie02@users.noreply.github.com>
2021-12-11 14:56:29 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -2,8 +2,8 @@
 
 float max( const float numeri[], int dim){
     numeri[dim];
-    float max = numeri[0];
-    for(i=0; i<dim; i++){
+    float max=numeri[0];
+    for(int i=0; i<dim; i++){
         if(max<numeri[i]){
             max=numeri[i];
         }
